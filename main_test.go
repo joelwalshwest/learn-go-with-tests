6d6b0de1d10cd539f4e98dcabe04ef18a38058ff
Joel Walsh-West <joelwalshwest@gmail.com>
2024-08-17 17:58:04 -0400

    Completion of Module 1

--- main_test.go
+++ main_test.go
@@ -3,9 +3,30 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Joel", "")
+		want := "Hello, Joel"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Bob", "French")
+		want := "Bonjour, Bob"
+		assertCorrectMessage(t, got, want)
+	})
+}
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
